a82e6319d016a43cef2ab67fcf9f5430e455c621
Ian McNichols <bmcnichols@arete.com>
2022-12-01 09:58:15 -0700

    Switching to list

--- 12_01/.12_1a.py
+++ 12_01/.12_1a.py
@@ -1,16 +1,20 @@
 def get_fat_elf():
     elf_file = open("../../elf_input.txt", "r")
-    highest_elf_cals = 0
+    calorie_counts = []
     cal_count = 0
     for line in elf_file.readlines():
         if line != "\n":
             cal_count += int(line)
         else:
-            if cal_count > highest_elf_cals:
-                highest_elf_cals = cal_count
+            calorie_counts.append(cal_count)
             cal_count = 0
     elf_file.close()
-    print("Highest calorie count:", highest_elf_cals)
+    calorie_counts.sort()
+    calorie_counts = calorie_counts[::-1]
+    print("Highest calorie count:", calorie_counts[0])
+    print("Second calorie count:", calorie_counts[1])
+    print("Third calorie count:", calorie_counts[2])
+    print("Sum of 3:", sum(calorie_counts[0:3]))
 
 
 get_fat_elf()
