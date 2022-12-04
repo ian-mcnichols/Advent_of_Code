7cc417942ffc7480794e5d1ab62492fadd28ea61
Ian McNichols <ian.mcnichols@gmail.com>
2022-12-03 22:10:54 -0700

    Initial solution

--- 12_04/12_04.py
+++ 12_04/12_04.py
@@ -7,8 +7,47 @@ def get_input():
 
 def challenge():
     lines = get_input()
+    overlaps = 0
+    for line in lines:
+        overlap1 = True
+        overlap2 = True
+        elf1 = line.split(",")[0]
+        elf2 = line.split(",")[1]
+        elf1_tasks = list(range(int(elf1.split("-")[0]), int(elf1.split("-")[1])+1))
+        elf2_tasks = list(range(int(elf2.split("-")[0]), int(elf2.split("-")[1])+1))
+        for task in elf1_tasks:
+            if task not in elf2_tasks:
+                overlap1 = False
+        for task in elf2_tasks:
+            if task not in elf1_tasks:
+                overlap2 = False
+        if overlap1 or overlap2:
+            overlaps += 1
+    print("overlaps:", overlaps)
+
+    return
+
+def challenge2():
+    lines = get_input()
+    overlaps = 0
+    for line in lines:
+        overlap1 = False
+        overlap2 = False
+        elf1 = line.split(",")[0]
+        elf2 = line.split(",")[1]
+        elf1_tasks = list(range(int(elf1.split("-")[0]), int(elf1.split("-")[1])+1))
+        elf2_tasks = list(range(int(elf2.split("-")[0]), int(elf2.split("-")[1])+1))
+        for task in elf1_tasks:
+            if task in elf2_tasks:
+                overlap1 = True
+        for task in elf2_tasks:
+            if task in elf1_tasks:
+                overlap2 = True
+        if overlap1 or overlap2:
+            overlaps += 1
+    print("overlaps:", overlaps)
 
     return
 
 
-challenge()
+challenge2()
