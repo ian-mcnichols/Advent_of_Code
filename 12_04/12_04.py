8b3eedf1f59150defad886af7517a8586f5762cb
Ian McNichols <ian.mcnichols@gmail.com>
2022-12-03 21:50:45 -0700

    skeleton for today's puzzle

---
+++ 12_04/12_04.py
@@ -0,0 +1,14 @@
+def get_input():
+    elf_file = open("elf_input.txt", "r")
+    elf_input = [x.strip() for x in elf_file.readlines()]
+    elf_file.close()
+    return elf_input
+
+
+def challenge():
+    lines = get_input()
+
+    return
+
+
+challenge()
